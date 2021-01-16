b85e9458894d3f3c2b564782dde88dfd212f864f
Max Beneke <max.beneke@gmail.com>
2021-01-16 11:15:01 -0500

    commit tree 95e504bccd9ce875fe6b9f4e9de9a0707d61fed1

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,13 @@
 class Instructor
+    @@all = []
+    def self.all
+        @@all
+    end
 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
 
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    attr_reader :first_name
+
+    def initialize(first_name)
+        @first_name = first_name
+        self.class.all << self
+    end
+
+    def all_instructors
+    end
+
+    def find_student(name)
+        self.all.find {|student| student.first_name == name}
+    end
+
+
+
 end
